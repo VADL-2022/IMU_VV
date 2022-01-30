5e28298643c26d80a15895b0caa52e5c1280eb9c
Kai <kdmalc@gmail.com>
2022-01-29 18:34:50 -0600

    IMU fixed

--- 04_IMU_Testing/alternatives/LIS331_loop.py
+++ 04_IMU_Testing/alternatives/LIS331_loop.py
@@ -43,20 +43,47 @@ bus = smbus.SMBus(1)
 # Select control register 1, 0x20(32)
 #		0x27(39)	Power ON mode, Data output rate = 50 Hz
 #					X, Y, Z-Axis enabled
-bus.write_byte_data(0x18, 0x20, 0x27)
+address=0x19
+bus.write_byte_data(address, 0x20, 0x27)
 # Required Binary: 001 (normal power mode), 00 (50 Hz), 111 (enable XYZ)
 # Equivalent Hex: 00100111 -> 0x27
 # TOGGLE THE MAXIMUM RANGE
 ###############################################################################
 # H3LIS331DL address, 0x18(24)
 # Select control register 4, 0x23(35)
-#		0x00(00)	Continuous update, Full scale selection = +/-24g
+#		0x00(00)	Continuous update,
+#Full scale selection = +/-24g: 0x18
+#SUBSCALE = +/- 12g: 0x10
 #bus.write_byte_data(0x18, 0x23, 0x00)
-bus.write_byte_data(0x18, 0x23, 0x18)
+bus.write_byte_data(address, 0x23, 0x10)
 
 time.sleep(0.5)
 
-while True:
+avgX=0
+avgY=0
+avgZ=0
+counter=0
+
+offsetX=0
+offsetY=0
+offsetZ=0
+
+def calibrate(file):
+    global offsetX
+    global offsetY
+    global offsetZ
+    with open(file, 'r') as f:
+        for line in f:
+            pass
+        last_line = line
+        offsetX,offsetY,offsetZ=list(map(float, last_line.split(",")))
+
+def runOneIter():
+    global avgX
+    global avgY
+    global avgZ
+    global counter
+    
     '''Do I ever need to sleep here? Or will the while take care of it?'''
     
     # X AXIS
@@ -64,8 +91,8 @@ while True:
     # H3LIS331DL address, 0x18(24)
     # Read data back from 0x28(40), 2 bytes
     # X-Axis LSB, X-Axis MSB
-    data0 = bus.read_byte_data(0x18, 0x28)
-    data1 = bus.read_byte_data(0x18, 0x29)
+    data0 = bus.read_byte_data(address, 0x28)
+    data1 = bus.read_byte_data(address, 0x29)
     
     # Convert the data
     xAccl = data1 * 256 + data0
@@ -78,8 +105,8 @@ while True:
     # H3LIS331DL address, 0x18(24)
     # Read data back from 0x2A(42), 2 bytes
     # Y-Axis LSB, Y-Axis MSB
-    data0 = bus.read_byte_data(0x18, 0x2A)
-    data1 = bus.read_byte_data(0x18, 0x2B)
+    data0 = bus.read_byte_data(address, 0x2A)
+    data1 = bus.read_byte_data(address, 0x2B)
     
     # Convert the data
     yAccl = data1 * 256 + data0
@@ -92,8 +119,8 @@ while True:
     # H3LIS331DL address, 0x18(24)
     # Read data back from 0x2C(44), 2 bytes
     # Z-Axis LSB, Z-Axis MSB
-    data0 = bus.read_byte_data(0x18, 0x2C)
-    data1 = bus.read_byte_data(0x18, 0x2D)
+    data0 = bus.read_byte_data(address, 0x2C)
+    data1 = bus.read_byte_data(address, 0x2D)
     
     # Convert the data
     zAccl = data1 * 256 + data0
@@ -106,6 +133,18 @@ while True:
     #print("Acceleration in X-Axis : %d" %xAccl)
     #print("Acceleration in Y-Axis : %d" %yAccl)
     #print("Acceleration in Z-Axis : %d" %zAccl)
-    
-    my_accels = [xAccl, yAccl, zAccl]
+
+    avgX += xAccl
+    avgY += yAccl
+    avgZ += zAccl
+    counter += 1
+    my_accels = [(xAccl-offsetX)/1000.0*9.81, (yAccl-offsetY)/1000.0*9.81, (zAccl-offsetZ)/1000.0*9.81]
     append_list_as_row(my_log, my_accels)
+
+calibrate("LOG_20220129-182509.csv")
+try:
+    while True:
+        runOneIter()
+finally:
+    if counter > 0:
+        append_list_as_row(my_log, [avgX/counter,avgY/counter,avgZ/counter])
