3f29500c7826f3c2ecc267858c004373d18a96b8
kdmalc <kdmalc@gmail.com>
2022-04-28 21:16:27 -0500

    Updated SLI Moving Averages

--- 05_NASA_SLI/YPR_funcs.py
+++ 05_NASA_SLI/YPR_funcs.py
@@ -1,5 +1,6 @@
 import numpy as np
 from math import sin, cos, pi, atan2, asin, acos
+import matplotlib.pyplot as plt
 
 def sind(x):
     return sin(np.deg2rad(x))
@@ -12,6 +13,61 @@ def tand(x):
 
 def acosd(x):
     return np.rad2deg(acos(x))
+
+
+def calc_ffangle_arrays(t_array, alpha_array, beta_array, gamma_array, t_apogee=17.21):
+    N = len(t_array)
+    
+    # Preallocate
+    angle_array = np.zeros(N)  # angle
+    n_array = np.zeros((3,N))  # axis
+    theta_array = np.zeros(N)  # resolved angle
+    
+    ## Calculate
+    # iterate until apogee
+    for i in range(N):
+
+        alpha = alpha_array[i]
+        beta = beta_array[i]
+        gamma = gamma_array[i]
+
+        R, angle, theta = convert_fftheta(alpha, beta, gamma)
+        angle_array[i] = angle
+
+        # theta
+        #theta = acosd(np.dot(R[:,2], [0, 0, 1]))
+        theta_array[i] = theta
+
+    # Theta vs Time
+    pitch_array = -1*theta_array
+    pitch_array = pitch_array - pitch_array[0]
+    t_pre_apog = t_array[t_array < t_apogee]
+    i_apogee = t_pre_apog.shape[0]
+    pitch_array_apog = pitch_array[0:i_apogee]
+
+    # Pitch vs Time
+    gamma_array_apog = gamma_array[0:i_apogee]
+    
+    return t_pre_apog, pitch_array_apog, gamma_array_apog
+
+
+def make_pitch_plot(t_pre_apog, pitch_array_apog):
+    # Theta vs. Time
+    fig1 = plt.figure(1, figsize=(8,8))
+    ax = plt.subplot(111)
+    l1 = ax.plot(t_pre_apog, pitch_array_apog, color='black', label='SLI Flight')
+    l2 = plt.axvline(x=2.5, color='blue', linestyle="--", label='Wind Shear (at about 1500 ft)')
+    l3 = plt.axvline(x=17.5, color='red', linestyle="--", label='Apogee Reached')
+    plt.grid()
+    #plt.yticks(np.linspace(0,90, num=11))
+    #plt.ylim((0, 90))
+    #plt.xticks(np.linspace(0,18, num=10))
+    ax.set_title("Pitch Angle vs. Time (Until Apogee)")
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel("Theta: Pitch Angle [deg]")
+    ax.legend(loc=2)
+    plt.show()
+    
 
 def convert_ffYPR_matrix(alpha, beta, gamma):
     '''
@@ -71,7 +127,7 @@ def convert_abg(bYPR):
     return np.array([alpha, beta, gamma])
 
 
-def convert_YPR_fixed_frame(alpha, beta, gamma):
+def convert_fftheta(alpha, beta, gamma):
     '''
     This function returns the YPR in the fixed world frame.
     Data collected from the Vector Nav is in the body frame and must be transformed
@@ -101,4 +157,4 @@ def convert_YPR_fixed_frame(alpha, beta, gamma):
     # theta
     theta = acosd(np.dot(R[:,2], [0, 0, 1]))
 
-    return R #, angle, theta
+    return R, angle, theta
